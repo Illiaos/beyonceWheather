528b8692277895c15eea78607ba28c6ad94f1e26
TehWalrusCode <sk8r4life12@hotmail.com>
2024-03-14 18:54:14 +0000

    Initial Commit - Added dependencies and example program on OWM api

---
+++ src/main/java/OWM_example.java
@@ -0,0 +1,23 @@
+import net.aksingh.owmjapis.core.OWM;
+import net.aksingh.owmjapis.api.APIException;
+import net.aksingh.owmjapis.model.CurrentWeather;
+
+public class OWM_example {
+
+    public static void main(String[] args)
+            throws APIException {
+
+        // declaring object of "OWM" class
+        OWM owm = new OWM("YOUR-API-KEY-HERE");
+
+        // getting current weather data for the "London" city
+        CurrentWeather cwd = owm.currentWeatherByCityName("London");
+
+        //printing city name from the retrieved data
+        System.out.println("City: " + cwd.getCityName());
+
+        // printing the max./min. temperature
+        System.out.println("Temperature: " + cwd.getMainData().getTempMax()
+                + "/" + cwd.getMainData().getTempMin() + "\'K");
+    }
+}
