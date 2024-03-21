9f7b33ab022e9bb7f3a918c2779bfea4dbfcf79b
Illia <movchan708@gmail.com>
2024-03-21 16:43:07 +0000

    Add API Key, modify gitignore

--- src/main/java/OWM_example.java
+++ src/main/java/OWM_example.java
@@ -2,13 +2,14 @@ import net.aksingh.owmjapis.core.OWM;
 import net.aksingh.owmjapis.api.APIException;
 import net.aksingh.owmjapis.model.CurrentWeather;
 
-public class OWM_example {
-
-    public static void main(String[] args)
-            throws APIException {
+public class OWM_example
+{
+    private static final String apiKey = "a062d298e14f66a96d308541cf16cf6f";
+    public static void main(String[] args) throws APIException
+    {
 
         // declaring object of "OWM" class
-        OWM owm = new OWM("YOUR-API-KEY-HERE");
+        OWM owm = new OWM(apiKey);
 
         // getting current weather data for the "London" city
         CurrentWeather cwd = owm.currentWeatherByCityName("London");
