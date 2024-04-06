b5ade9df77309bbb5c095b0756093bf2cb0844a9
TehWalrusCode <sk8r4life12@hotmail.com>
2024-04-06 20:27:25 +0100

    Added chatbot - Updated StartupManager to launch the bot

--- src/main/java/manages/WeatherManager.java
+++ src/main/java/manages/WeatherManager.java
@@ -10,7 +10,7 @@ import java.net.URL;
 
 
 import org.json.JSONArray;
-import  org.json.JSONObject;
+import org.json.JSONObject;
 import weather_description.WeatherTemperature;
 
 public class WeatherManager {
