e199b6a82523dabf4dfb10debc57a25b3d5abd93
juliaklein <juliakb1@hotmail.com>
2024-04-17 18:32:16 +0100

    code and comments for UserRequest

--- src/main/java/user_input/UserRequest.java
+++ src/main/java/user_input/UserRequest.java
@@ -1,4 +1,37 @@
 package user_input;
+import java.util.Date;
 
 public class UserRequest {
+
+    private Date startDate;
+    private String[] requestCity;
+
+    // default constructor; not finished
+    private UserRequest() {
+
+    }
+
+    // constructor to initialize date and requested cities
+    private UserRequest(Date date, String[] requestCity) {
+        this.startDate = date;
+        this.requestCity = requestCity;
+    }
+
+    // getter and setters for start date
+    public Date getStartDate() {
+        return this.startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = date;
+    }
+
+    public String[] getRequestCity() {
+        return this.requestCity;
+    }
+    // getter and setters for city
+    public void setRequestCity(String[] city) {
+        this.requestCity = city;
+    }
+
 }
