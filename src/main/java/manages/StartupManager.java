853e6c13db0ffa82bf8b0c06bbb2952049722b38
TehWalrusCode <sk8r4life12@hotmail.com>
2024-04-29 17:54:29 +0100

    updated bot to accept more kinds of weather prompts

--- src/main/java/manages/StartupManager.java
+++ src/main/java/manages/StartupManager.java
@@ -1,6 +1,14 @@
 package manages;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+
+import java.awt.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.*;
+
+import city.City;
 
 import org.alicebot.ab.*;
 import org.alicebot.ab.utils.IOUtils;
@@ -55,23 +63,53 @@ public class StartupManager
                     String question = "";
                     String[] requestArr = request.split(" ");
 
-                    //forloop to extract question from request
+                    //for loop to extract question from request
                     for(int i = 0; i < requestArr.length - 1; i++){
 
                         question += requestArr[i] + " ";
-                        question.strip();
 
                     }
+
+                    question = question.strip();
 
                     //location to be queried
                     String location = requestArr[requestArr.length - 1];
 
                     //if the question matches the set structure
-                    //TODO - Create more variations of questions
-                    if(question.toUpperCase() == "WHAT IS THE WEATHER LIKE IN"){
+                    if(correctSyntax(question.toUpperCase())){
 
                         //request the weather
-                        manager.requestWeather(location);
+                        City city = manager.requestWeather(location);
+                        String recommendedClothes = "";
+
+                        //temps lower than 0
+                        if(city.getTemperature() <= 0) {
+                            recommendedClothes = "a parka, woolen pants, some warm gloves, and a wooly hat.";
+                        }
+                        //temps between 0 and 10
+                        else if(city.getTemperature() <= 10){
+                            recommendedClothes = "a thick jacket or fleece, woolen pants, and a scarf.";
+                        }
+                        //temps between 10 and 20
+                        else if(city.getTemperature() <= 20){
+                            recommendedClothes = "a lightweight cardigan, some jeans, and maybe a scarf.";
+                        }
+                        //temps between 20 and 30
+                        else if(city.getTemperature() <= 30){
+                            recommendedClothes = "a lightweight top such as a T-shirt, shorts or light pants, a sun cap, sandals, and sunglasses. It might also be beneficial to bring sunscreen and a water bottle.";
+                        }
+                        //temps between 30 and 40
+                        else if(city.getTemperature() <= 40){
+                            recommendedClothes = "light, breathable, and loose fitting clothes such as summer dresses or T-shirts, shorts, and sandals. It would be helpful to bring a water bottle and sunscreen as well to keep cool and protect against UV exposure.";
+                        }
+                        //temps above 40
+                        else{
+                            recommendedClothes = "light, breathable clothes, a lot of sunscreen, a good amount of water to stay hydrated, and someplace to seek shade until temperatures cool.";
+                        }
+
+                        String response = "As the temperature in " + city.name + " is currently " + city.getTemperature() + "° Celsius, I would recommend " + recommendedClothes;
+
+                        System.out.println(">" + response);
 
                     }
                     //else run bot code
@@ -97,6 +135,23 @@ public class StartupManager
         } catch(Exception e){
             e.printStackTrace();
         }
+    }
+
+    private static boolean correctSyntax(String question) {
+
+        String[] questionArr = {"WHAT IS THE WEATHER LIKE IN", "WHAT'S THE WEATHER LIKE IN", "WHATS THE WEATHER LIKE IN",
+                                "WHAT IS THE WEATHER IN", "WHAT'S THE WEATHER IN", "WHATS THE WEATHER IN",
+                                "WHAT IS THE TEMPERATURE LIKE IN", "WHAT'S THE TEMPERATURE LIKE IN", "WHATS THE TEMPERATURE LIKE IN",
+                                "WHAT IS THE TEMPERATURE IN", "WHAT'S THE TEMPERATURE IN", "WHATS THE TEMPERATURE IN",
+                                "WHAT IS IT LIKE IN", "WHAT'S IT LIKE IN", "WHATS IT LIKE IN",
+                                "WHAT IS THE FORECAST FOR", "WHAT'S THE FORECAST FOR", "WHATS THE FORECAST FOR"};
+
+        for(int i = 0; i < questionArr.length; i++){
+            if(question.equals(questionArr[i]))
+                return true;
+
+        }
+        return false;
     }
 
     private static String getResourcesPath() {
